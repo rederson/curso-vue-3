531c959ee0c08940d1366ea302bf9181f834168f
rederson <r.ataliba@hotmail.com>
2023-05-07 13:59:50 -0300

    options-api first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+
+{{ count }}
+{{ userName }}
+
+<ul>
+  <li v-for="user in users">{{  user.firstName }} - {{ user.age }}</li>
+</ul>
+
+  <div v-if="showHeader">
+  <Header />
+</div>
+
+<button v-on:click="showHeader = !showHeader" >Toggle header</button>
+
+  <h2>App</h2>
+
+<router-link to="/">Home</router-link>
+<router-link to="/about">About</router-link>
+
+<router-view></router-view>
+
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+components:{Header},
+  data() {
+    return {
+      count:0,
+      showHeader:false,
+      userName:'Alexandre',
+      users:[
+        {
+        firstName:'Alexandre',
+        age:40,
+      },
+      {
+        firstName:'Maria',
+        age:34,
+      }
+      ]
+    }
+  },
+  mounted(){
+    console.log('mounted');
+  },
+  updated() {
+    console.log('updated');
+  }
+}
+
+
+</script>
+<style>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="home">
+    <h1>Home</h1>
+  </div>
+  </main>
+</template>
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
