c76f6dc87f296ebf9c7a4fcab746ef1a81ac62c5
rederson <r.ataliba@hotmail.com>
2023-05-08 18:18:39 -0300

    Renderização condicional option-api

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstName',
+        'lastName',
+        'is_admin',
         'email',
         'password',
     ];
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,40 +1,67 @@
 <template>
+  <Header v-if="showHeader" />
+  <hr />
+
+  <template v-if="showHeader">
+    <Header />
+  </template>
+
+  <hr />
+
   <h2>Lista de Users</h2>
 
   <ul>
-    <li v-for="user in users" :key="user.id">{{ user.firstName }}</li>
+    <template v-for="user in users" :key="user.id">
+      <li v-if="user.is_admin === 1">
+        {{ user.firstName }}
+      </li>
+    </template>
   </ul>
- 
+
+<hr />
+
+<ul>
+    <li v-for="user in users" :key="user.id">
+      {{ user.firstName }}
+      <button v-if="user.is_admin">Change to not admin</button>
+      <button v-else>Change to admin</button>
+    </li>
+  </ul>
+
+
+
+
+
+
 </template>
 
-<script> 
-import http from '@/services/http.js';
+<script>
+import http from "@/services/http.js";
+import Header from "@/components/Header.vue";
 
 export default {
-  
- data(){
-  return {
-    users: []
+  components: { Header },
+  data() {
+    return {
+      users: [],
+      showHeader: false,
+    };
+  },
 
-  }
- },
-
- async mounted() {
-  try {
- const {data} = await http.get('api/users');
- //console.log(data);
- this.users = data;
-  } catch (error) {
-       console.log(error);
-  }
- },
- updated(){
-  console.log('upadated');
- }
-
-}
+  async mounted() {
+    try {
+      const { data } = await http.get("api/users");
+      //console.log(data);
+      this.users = data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  updated() {
+    console.log("upadated");
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
