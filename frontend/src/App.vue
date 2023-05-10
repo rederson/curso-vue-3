7c94dbd7b745be866857eb96ed47ae6ae940cd2e
rederson <r.ataliba@hotmail.com>
2023-05-10 17:59:03 -0300

    Form dinâmico composition-api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,57 +1,37 @@
-<script setup>
-
-import http from '@/services/http.js';
-
-import { onMounted, reactive, ref } from "vue";
-
-let users = reactive({users:[]});
-const imageSrc = ref('https://picsum.photos/200/300');
-const is_admin = ref(false);
-const count = ref(0);
-const userName= ref('');
-
-function add(teste) {
-  console.log(teste);
-  count.value++
-}
-
-function changeName() {
-  userName.value = event.target.value;
-}
-
-onMounted( async ()=>{
-  try {
-    const {data} = await http.get('api/users');
-    //console.log(data);
-    users.users = data;
-  } catch (error) {
-    console.log(error);
-  }
-})
-</script>
 <template>
-  <h2>Lista de users</h2>
-  
-  <img :src="imageSrc" alt="" class="my-default-class" :class="{'my-class':is_admin,'my-other-class':!is_admin}" />
+  <input type="text" v-model="userName">
 
- <button @click="add('teste')">Clique aqui {{ count }}</button>
+  <template v-for="user in users">
+    <input type="checkbox" @click="userName = user.firstName" :checked="userName === user.firstName">{{ user.firstName }}
+  </template>
 
- {{ userName }}
- 
-<!--<input type="text" @keyup="changeName" placeholder="Name">-->
-<input type="text" v-model="userName" placeholder="Name">
-  <ul>
-    <li v-for="user in users.users" :key="user.id">{{ user.firstName }}</li>
-  </ul>
+
+<select name="" v-model="userName">
+<option :value="user.firstName" v-for="user in users">{{ user.firstName }}</option>
+</select>
 
 </template>
 
-<style scoped>
-.my-class {
-  border: solid 4px red;
-}
-.my-other-class {
-  border: solid 4px blue;
-}
+<script setup>
+import { reactive, ref } from "vue";
 
+const users = reactive([
+  {
+    id: 1,
+    firstName: "Alexandre",
+  },
+  {
+    id: 2,
+    firstName: "João",
+  },
+  {
+    id: 3,
+    firstName: "Maria",
+  },
+]);
+
+const userName = ref("Alexandre");
+</script>
+
+<style scoped>
 </style>
