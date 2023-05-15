445ae4f12fa28969645b1dfe7b38e722c36e0f72
rederson <r.ataliba@hotmail.com>
2023-05-15 15:03:46 -0300

    Scoped Slot com a Options API

---
+++ frontend/src/components/Products.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <slot v-for="(product,index) in products" :key="index" v-bind:product="product"></slot>
+
+
+</template>
+
+<script>
+export default {
+props:['products'],
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,30 +1,39 @@
 <template>
-  <h2 id="app-products">Products</h2>
-  <h2 id="my-app">Products my app</h2>
-
-  <h2>Product 1</h2>
-  <p>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ipsam
-    doloribus itaque iure quo, ea aspernatur in dolores expedita cumque, odit
-    eaque ipsa libero optio aliquam nobis! Ullam, cupiditate magnam?
-  </p>
-  <ProductInfo :likes="product.likes" :comments="product.comments" />
+  <Products v-bind:products="products" v-slot="props">
+    <li @click="getProduct(props.product.id)">{{ props.product.name }}</li>
+  </Products>
 </template>
 
 <script>
 import ProductInfo from "@/components/ProductInfo.vue";
+import Products from "@/components/Products.vue";
 
 export default {
-  components: { ProductInfo },
+  components: { ProductInfo, Products },
   name: "Products.vue",
   data() {
     return {
-        product: {
-            likes: 20,
-            comments: 30,
-        }
-    }
-  }
+      products: [
+        {
+          id: 1,
+          name: "Imac",
+        },
+        {
+          id: 2,
+          name: "MacBook",
+        },
+        {
+          id: 3,
+          name: "Mouse",
+        },
+      ],
+    };
+  },
+  methods: {
+    getProduct(id) {
+      console.log(id);
+    },
+  },
 };
 </script>
 
