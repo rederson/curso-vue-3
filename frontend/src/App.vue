4472943e2068230121413c2971f6850c9a76ad35
rederson <r.ataliba@hotmail.com>
2023-05-15 13:16:01 -0300

    Passando Evento de filho para pai composition-api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,25 @@
 
   <router-view></router-view>
 
+  <button @click="count++">Add count - {{ count }}</button>
+  <!--<CountChild v-on:addCount="count++" />-->
+
+  <!--<CountChild v-on:addCount="(value) => count+=value" />-->
+
+    <CountChild v-on:addCount="add" />
+
 </template>
 
 <script setup>
 import Nav from '@/components/Nav.vue';
+import CountChild from '@/components/CountChild.vue';
+import { ref } from 'vue';
 
+const count = ref(0);
+
+function add(value) {
+  count.value+=value
+}
 </script>
 
 <style scoped>
---
+++ frontend/src/components/CountChild.vue
@@ -0,0 +1,22 @@
+<template>
+<!--<button @click="$emit('addCount')">Add count from child</button>-->
+
+<button @click="add">Add count from child</button>
+
+</template>
+
+<script setup>
+const emit = defineEmits(['addCount']);
+
+function add() {
+    emit('addCount', 10);
+}
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
