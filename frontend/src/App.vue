48264ae0b5a7bd8cff6475884fa1581f31416950
rederson <r.ataliba@hotmail.com>
2023-05-17 16:34:26 -0300

    A importância de um key único no v-for

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <Nav />
+  <!--<Nav />-->
 
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
 
+<button @click="addItem">Add</button>
+
+<ul>
+  <li v-for="(item, index) in items" :key="item">
+    {{ item }}
+    <input placeholder="Qualquer coisa">
+    <button v-on:click="remove(index)">Remove</button>
+   
+   
+
+  </li>
+</ul>
 
 </template>
+
+
+
 <script>
 import Nav from "@/components/Nav.vue";
 import CountChild from "@/components/CountChild.vue";
@@ -15,15 +30,8 @@ export default {
   data() {
     return {
       count: 0,
-      itens: [
-        {
-          name: 'teste'
-        }, 
-        {
-          name: 'teste1'
-        }
-      ]
-    };
+      items: [ ]
+    }
   },
   mounted(){
     //console.log(this.$refs.img['src']);
@@ -32,6 +40,20 @@ export default {
     add(value) {
       this.count += value;
     },
+
+    addItem(){
+      this.items.push(Date.now())
+    },
+
+    remove(index){
+      console.log(index);
+     // const index = this.itens.findIndex((i) => {
+     //   return i.id === item.id;
+     // });
+     this.items.splice(index,1);
+     //console.log(index);
+    },
+
 
     search: _.debounce(()=>{
           console.log('search');
