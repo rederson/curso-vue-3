00a0586f9a42b4c5bb9371b50555f9c014e12cdc
rederson <r.ataliba@hotmail.com>
2023-05-18 17:58:34 -0300

    NextTick options-api

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,64 +1,35 @@
 <template>
-<CurrencyInput type="text" v-model="dolar" placeholder="Dolar" :options="{currency: 'USD'}" />
-<hr>
-<ul>
-  <li>{{ dolarTodayValue }}</li>
-    <li>{{ dolarToReaisValue }}</li>
-</ul>
+  <button @click="showSomething()">Show</button>
+
+  <input type="text" ref="inputElement" v-if="showElement">
 </template>
 
 <script>
-import format from '../services/format';
-import http from '../services/http';
-import CurrencyInput from '@/components/CurrencyInput.vue';
 export default {
-  components: {
-    CurrencyInput
-  },
   data() {
     return {
-      dolar: 0,
-      dolarToday:0,
-      dolarToReais:0,
+        showElement:false,
     }
   },
+  methods: {
+    async showSomething(){
+      //console.log('show');
+      this.showElement =  true;
+      
+      await this.$nextTick(()=>{
+        this.$refs.inputElement.focus();
+        console.log('focus');
+      })
+      console.log('carregou');
+/** ou trabalhar com setTimeout */
+     /** setTimeout(() => {
+        this.$refs.inputElement.focus();
+        console.log(this.$refs.inputElement);
+      }, 100);
+*/
 
-  async mounted(){
-    try {
-      const dolarToday = await this.getDolar();
-      this.dolarToday =  dolarToday['high'];
-    } catch (error) {
-      console.log(error.response.data);
-    }
-
-  },
-
-
-  methods:{
-    async getDolar(type = 'USDBRL'){
-      const {data} = await http.get('https://economia.awesomeapi.com.br/json/last/USD-BRL');
-      const typeCurrency = type.split('-').join();
-      return data[typeCurrency];
-    }
-  },
-
-  computed:{
-    dolarToReaisValue(){
-      return `O valor em reais de ${format(this.dolar,'en-US','USD')} é de: ${format(this.dolarToReais,'pt-BR','BRL')} `;
-    },
-
-    dolarTodayValue(){
-      return `O dolar hoje está em: ${format(this.dolarToday,'en-US','USD')}`;
-      }
-  },
-
-  watch:{
-    dolar(value){
-
-      this.dolarToReais = value*this.dolarToday;
     }
   }
-
 }
 
 </script>
