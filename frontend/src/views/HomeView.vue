75b5057af68ab7dfaacddc980e91bfa10bd0c7da
rederson <r.ataliba@hotmail.com>
2023-05-17 21:13:09 -0300

    watch com options-api

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,82 +1,66 @@
 <template>
-  <input type="text" v-model="user.firstName" placeholder="First Name">
-  <input type="text" v-model="user.lastName" placeholder="Last Name">
-  <input type="text" v-model="user.address.street" placeholder="Street Address">
-  <hr>
-
-  <ul>
-    <li v-for="(item, index) in items" :key="index">
-    {{ item.name }} <input type="text" v-model="item.price">
-    </li>
-  </ul>
-
-
+<CurrencyInput type="text" v-model="dolar" placeholder="Dolar" :options="{currency: 'USD'}" />
+<hr>
+<ul>
+  <li>{{ dolarTodayValue }}</li>
+    <li>{{ dolarToReaisValue }}</li>
+</ul>
 </template>
 
 <script>
+import format from '../services/format';
+import http from '../services/http';
+import CurrencyInput from '@/components/CurrencyInput.vue';
 export default {
-  data(){
-    return {
-      firstName:'',
-      lastName:'',
-      user:{
-        firstName:'',
-        lastName: '',
-        address:{
-          street:''
-        }
-      },
-      items:[
-        {
-          id:1,
-          name: 'teste1',
-          price: 10
-        },
-        {
-          id:2,
-          name: 'teste2',
-          price: 100
-        }
-      ]
-    }
-  }, 
-  mounted(){
-    console.log('mounted')
+  components: {
+    CurrencyInput
   },
-  watch: {
+  data() {
+    return {
+      dolar: 0,
+      dolarToday:0,
+      dolarToReais:0,
+    }
+  },
 
-    // primitivos
-    firstName(value, oldValue){
-      console.log('new =>'+value,'old =>'+oldValue);
+  async mounted(){
+    try {
+      const dolarToday = await this.getDolar();
+      this.dolarToday =  dolarToday['high'];
+    } catch (error) {
+      console.log(error.response.data);
+    }
+
+  },
+
+
+  methods:{
+    async getDolar(type = 'USDBRL'){
+      const {data} = await http.get('https://economia.awesomeapi.com.br/json/last/USD-BRL');
+      const typeCurrency = type.split('-').join();
+      return data[typeCurrency];
+    }
+  },
+
+  computed:{
+    dolarToReaisValue(){
+      return `O valor em reais de ${format(this.dolar,'en-US','USD')} é de: ${format(this.dolarToReais,'pt-BR','BRL')} `;
     },
-    lastName(value, oldValue){
-      console.log('new =>'+value,'old =>'+oldValue);
-    },
-    //objetos
-    'user.firstName': function(value, oldValue){
-      console.log('new =>'+value,'old =>'+oldValue);
-    },
-    'user.lastName': function(value, oldValue){
-      console.log('new =>'+value,'old =>'+oldValue);
-    },
-    'user.address.street': function(value, oldValue){
-      console.log('new =>'+value,'old =>'+oldValue);
-    },
-    // objeto em array
-    items:{
-      handler(itemsObject, oldValue){
-     //   itemsObject.forEach(element => {
-     //     if (!Number.isInteger(Number(element.price))) {
-     //       console.log('Não é um integer '+element.price);
-     //     }
-     //   })
-     console.log('carregou');
-      },
-      deep:true,
-      immediate:true
+
+    dolarTodayValue(){
+      return `O dolar hoje está em: ${format(this.dolarToday,'en-US','USD')}`;
+      }
+  },
+
+  watch:{
+    dolar(value){
+
+      this.dolarToReais = value*this.dolarToday;
     }
   }
-};
+
+}
+
 </script>
 
 <style >
