78c0855e62c97eae35112ba40e4b47df7aaa65d3
rederson <r.ataliba@hotmail.com>
2023-05-19 13:16:38 -0300

    teleport na Composition API

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -1,17 +1,8 @@
 <template>
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-content">
-        <div class="box">
-            <slot name="header"></slot>
-            <slot />
-
-            <slot name="footer"></slot>
-        </div>
-      
-    </div>
-    <button class="modal-close is-large" aria-label="close" @click="close"></button>
-  </div>
+  <div>
+    <h2>Modal</h2>
+      <slot />
+  </div> 
 </template>
 
 <script setup>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,35 +1,58 @@
 <template>
+  <!--
+    <div>
   <button @click="showSomething()">Show</button>
- 
- <input type="text" v-if="showElement" ref="inputElement">
+  </div>
+
+ <teleport to="#input">
+   <input type="text" ref="inputElement">
+ </teleport>
+
+<teleport to="#lorem">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nobis id
+  officia laborum quae cumque rerum molestiae officiis. Provident ad quis
+  mollitia sunt exercitationem, voluptatum dolores possimus sint ea rem?
+-->
+<div>
+  <button @click="open=!open" >Show</button>
+</div>
+
+<teleport to="#modal">
+<Modal v-if="open">
+  Conteúdo do modal vai aqui
+</Modal>
+</teleport>
 </template>
 <script setup>
-
 import { nextTick, ref } from "vue";
+import Modal from "@/components/Modal.vue";
 
 const showElement = ref(false);
 const inputElement = ref(null);
+const open = ref(false);
 
 async function showSomething() {
   showElement.value = true;
 
   setTimeout(() => {
-      inputElement.value.focus()
+    inputElement.value.focus();
 
-        console.log(inputElement.value);
+    console.log(inputElement.value);
   }, 100);
 
-await nextTick(()=>{
-  inputElement.value.focus()
-  //console.log(inputElement.value);
-  console.log('focus');
-})
+  await nextTick(() => {
+    inputElement.value.focus();
+    //console.log(inputElement.value);
+    console.log("focus");
+  });
 
-  console.log('carregou');
+  console.log("carregou");
 }
 
 </script>
 
-<style scoped>
-
+<style>
+#app {
+  color: red;
+}
 </style>
