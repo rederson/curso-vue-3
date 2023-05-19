4265dc2c9ed71caf399ae38909afd39434a15d6a
rederson <r.ataliba@hotmail.com>
2023-05-19 19:00:05 -0300

    VUEX - Ciclo do state em componente - Options API

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,32 +2,26 @@
   <button @click="increment" >Add</button>
   {{ count }} 
   
-  <hr>
-
-  <ul>
-    <li v-for="(user, index) in usersData" :key="index">{{ user.firstName }}</li>
-  </ul>
-{{ getTotalUsers }}
 <hr>
-{{ name }}
 
 </template>
 
 <script>
-import count from '@/mixins/count.js';
-import users from '@/mixins/users.js';
 
 export default {
-  mixins:[count, users],
+
   data(){
     return {
-      name: 'Alexandre'
+        count: 0,
     }
   },
   
-  mounted(){
-    console.log('chamou de dentro do mouted do componente home view');
+  methods:{
+    increment(){
+      this.count++;
+    }
   }
+  
 };
 </script>
 
